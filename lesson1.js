6701e5e54d6171dd2bc7b6930fdfb57dc0743f28
John Hankins <jrhankins883@yahoo.com>
2025-02-06 12:53:53 -0500

    This is the code so far for questions 1-81

--- lesson1.js
+++ lesson1.js
@@ -2,23 +2,23 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-  const test = "Testing";
-  console.log("This works!");
-  return test;
-  console.log("This works!");
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-const call = padRow("CamperChan");
-console.log(call);
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("false") {
+  console.log("Condition is true");
 }
 
 let result = ""
 
 for (const row of rows) {
-  result = result + row + "\n"
+  result = result + row + "\n";
 }
 
 console.log(result);
