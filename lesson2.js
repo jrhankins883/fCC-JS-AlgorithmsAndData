dc1cb6b1d2c04f090e11450c1d8b9b9bf90c38b7
John Hankins <jrhankins883@yahoo.com>
2025-02-24 18:13:59 -0500

    Second step of a four step problem

--- lesson2.js
+++ lesson2.js
@@ -1,3 +1,4 @@
+// Exercise #1
 function getAverage(scores) {
     let sum = 0;
     let average = 0;
@@ -10,3 +11,29 @@ function getAverage(scores) {
   
   console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
   console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+// Exercise #2
+function getGrade(score) { 
+  if (score == 100) {
+    return "A++";
+  }
+  else if (score >= 90) {
+    return "A";
+  }
+  else if (score >= 80) {
+    return "B";
+  }
+  else if (score >= 70) {
+    return "C";
+  }
+  else if (score >= 60) {
+    return "D";
+  }
+  else (score > 59); {
+    return "F";
+  }
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
